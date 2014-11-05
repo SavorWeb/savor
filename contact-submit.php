4dd3eb68039f69fef02ab3408c91e693600442e3
Ryan Maskell <Ryan@SavorWeb.com>
2014-11-04 17:14:36 -0800

    actual address

--- contact-submit.php
+++ contact-submit.php
@@ -7,17 +7,17 @@ $phone = strip_tags($_POST['phone']);
 $message = strip_tags($_POST['message']);
 
 // Send Message
-$success = mail( "maskellryan@gmail.com", "Savor Webmail:",
+$success = mail( "info@savorweb.com", "Savor Webmail:",
 "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message\n",
 "From: $name <$email>" );
 
 // redirect to success page 
 if ($success){
-  header ("Location: index.php");
+  header ("Location: /");
   exit;
 }
 else{
-  header ("Location: index.php");
+  header ("Location: /");
   exit;
 }
 ?>
