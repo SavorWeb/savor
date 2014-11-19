2376308609f425cf68dcf6a60c2a96f00183a1ab
Tim Herbert <timothyherbert@hotmail.com>
2014-11-18 17:39:33 -0800

    Captcha / SEO
    Added form Captcha and some SEO

--- contact-submit.php
+++ contact-submit.php
@@ -1,25 +1,31 @@
 <?php
 // Get Data 
-$name = strip_tags($_POST['name']);
-$email = strip_tags($_POST['email']);
-$phone = strip_tags($_POST['phone']);
+$captcha = strip_tags($_POST['captcha']);
+if ($captcha == 9517){
+	$name = strip_tags($_POST['name']);
+	$email = strip_tags($_POST['email']);
+	$phone = strip_tags($_POST['phone']);
 
-$message = strip_tags($_POST['message']);
+	$message = strip_tags($_POST['message']);
 
-// Send Message
-$success = mail( "info@savorweb.com", "Savor Webmail:",
-"Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message\n",
-"From: $name <$email>" );
+	// Send Message
+	$success = mail( "info@savorweb.com", "Savor Webmail:",
+	"Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message\n",
+	"From: $name <$email>" );
 
-// redirect to success page 
-if ($success){
-  header ("Location: /");
-  exit;
+	// redirect to success page 
+	if ($success){
+	  header ("Location: /");
+	  exit;
+	}
+	else{
+	  header ("Location: /");
+	  exit;
+	}
+
+}  else {
+	  header ("Location: /");
+	  exit;
 }
-else{
-  header ("Location: /");
-  exit;
-}
-?>
 
 ?>
